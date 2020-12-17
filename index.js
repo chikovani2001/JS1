2cc909e55ac188537cf868a2e27d12b2d08e1d38
chiko696969 <giorgi.chikovani.1@btu.edu.ge>
2020-12-17 14:28:24 +0400

    message

---
+++ index.js
@@ -0,0 +1,47 @@
+// function greaterNum(number1,number2){
+//     if(number1 > number2){
+//         return console.log( number1 + " " + "მეტია" +" "+ number2+"-ზე")
+//     }else{
+//         return console.log( number1 + " " + "ნაკლებია" +" "+ number2+"-ზე")
+//     }
+// }
+// console.log(greaterNum(3,5))
+// console.log(greaterNum(10,5))
+
+// function helloWorld(langugue){
+//     if(langugue == "en"){
+//         return console.log("Hello World!")
+//     }else if(langugue == "ka"){
+//         return console.log("გამარჯობა მსოფიოვ!")
+//     }else (langugue = null)
+//     {
+//         return console.log("Default is English")
+//     }
+// }
+// console.log(helloWorld("en"))
+// console.log(helloWorld("ka"))
+// console.log(helloWorld(" "))
+
+// function assignGrade(point){
+//     if( point <= 50){
+//         return console.log("Goodbye , you failed!")
+//     }else if (point <= 59){
+//         return console.log("F")
+//     }else if (point <= 69){
+//         return console.log("D")
+//     }else if (point<= 79){
+//         return console.log("C")
+//     }else if (point <= 90){
+//         return console.log("B")
+//     }else if (point <= 100){
+//         return console.log("A")
+//     }
+// }
+// console.log(assignGrade(5))
+// console.log(assignGrade(55))
+// console.log(assignGrade(65))
+// console.log(assignGrade(75))
+// console.log(assignGrade(85))
+// console.log(assignGrade(100))
+
+
